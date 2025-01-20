ffeb927bb59758a1c607005df61aeaf41af0e2a8
kapaza.local <kapaza@untels.edu.pe>
2025-01-20 17:05:34 -0500

    vista publica agregada

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 4500
+const port = 4500;
+const path = require('path');
 const Router = require('./src/routes/index.route.js');
 // middlewares
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 // Router
 app.use(Router);
 
